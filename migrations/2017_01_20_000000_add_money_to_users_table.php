0cf0b216cb63b265804970546619b0151079e302
shebaoting <th9th@th9th.com>
2024-09-02 13:50:23 +0800

    去掉数据表判断

--- migrations/2017_01_20_000000_add_money_to_users_table.php
+++ migrations/2017_01_20_000000_add_money_to_users_table.php
@@ -1,23 +1,7 @@
 <?php
 
 use Flarum\Database\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Builder;
 
-return [
-    'up' => function (Builder $schema) {
-        $schema->table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'money')) {
-                $table->integer('money')->default(0); // 根据需要设置默认值
-            }
-        });
-    },
-    'down' => function (Builder $schema) {
-        $schema->table('users', function (Blueprint $table) {
-            if (Schema::hasColumn('users', 'money')) {
-                $table->dropColumn('money');
-            }
-        });
-    }
-];
+return Migration::addColumns('users', [
+    'money' => ['integer']
+]);
