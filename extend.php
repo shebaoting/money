445ffba38587720c57f55c1a4bfdd93be9baf40c
折宝亭 <th9th@th9th.com>
2024-11-09 19:38:30 +0800

    修复编辑用户积分无效的bug

--- extend.php
+++ extend.php
@@ -45,7 +45,7 @@ return [
         ->serializeToForum('shebaoting-money.extra_char_points', 'shebaoting-money.extra_char_points'),
 
     (new Extend\Event())
-        ->listen(Saving::class, [Listeners\GiveMoney::class, 'postWillBeSaved']) // 修改这里
+        ->listen(SavingPostEvent::class, [Listeners\GiveMoney::class, 'postWillBeSaved']) // 修改这里
         ->listen(Posted::class, [Listeners\GiveMoney::class, 'postWasPosted'])
         ->listen(PostRestored::class, [Listeners\GiveMoney::class, 'postWasRestored'])
         ->listen(PostHidden::class, [Listeners\GiveMoney::class, 'postWasHidden'])
@@ -54,6 +54,7 @@ return [
         ->listen(DiscussionRestored::class, [Listeners\GiveMoney::class, 'discussionWasRestored'])
         ->listen(DiscussionHidden::class, [Listeners\GiveMoney::class, 'discussionWasHidden'])
         ->listen(DiscussionDeleted::class, [Listeners\GiveMoney::class, 'discussionWasDeleted'])
+        ->listen(Saving::class, [Listeners\GiveMoney::class, 'userWillBeSaved'])
         ->listen(Registered::class, [Listeners\GiveMoney::class, 'userWasRegistered'])
         ->listen(PostWasLiked::class, [Listeners\GiveMoney::class, 'postWasLiked'])
         ->listen(PostWasUnliked::class, [Listeners\GiveMoney::class, 'postWasUnliked']),
@@ -61,3 +62,50 @@ return [
     (new Extend\Routes('api'))
         ->get('/money-log', 'money.logs', UserMoneyLogsController::class)
 ];
+
+// $extend = [
+//     (new Extend\Frontend('forum'))
+//         ->js(__DIR__ . '/js/dist/forum.js')
+//         ->css(__DIR__ . '/less/forum.less')
+//         ->route('/money-log', 'user.money-log'),
+
+//     (new Extend\Frontend('admin'))
+//         ->js(__DIR__ . '/js/dist/admin.js')
+//         ->css(__DIR__ . '/less/admin.less'),
+
+//     new Extend\Locales(__DIR__ . '/locale'),
+
+//     (new Extend\ApiSerializer(UserSerializer::class))
+//         ->attributes(AddUserMoneyAttributes::class),
+
+//     (new Extend\Settings())
+//         ->serializeToForum('shebaoting-money.moneyname', 'shebaoting-money.moneyname')
+//         ->serializeToForum('shebaoting-money.level_names', 'shebaoting-money.level_names')
+//         ->serializeToForum('shebaoting-money.money_scale', 'shebaoting-money.money_scale')
+//         ->serializeToForum('shebaoting-money.noshowzero', 'shebaoting-money.noshowzero'),
+
+//     (new Extend\Event())
+//         ->listen(SavingPost::class, [Listeners\GiveMoney::class, 'postWillBeSaved'])
+//         ->listen(SavingDiscussion::class, [Listeners\GiveMoney::class, 'discussionWillBeSaved'])
+//         ->listen(Posted::class, [Listeners\GiveMoney::class, 'postWasPosted'])
+//         ->listen(PostRestored::class, [Listeners\GiveMoney::class, 'postWasRestored'])
+//         ->listen(PostHidden::class, [Listeners\GiveMoney::class, 'postWasHidden'])
+//         ->listen(PostDeleted::class, [Listeners\GiveMoney::class, 'postWasDeleted'])
+//         ->listen(Started::class, [Listeners\GiveMoney::class, 'discussionWasStarted'])
+//         ->listen(DiscussionRestored::class, [Listeners\GiveMoney::class, 'discussionWasRestored'])
+//         ->listen(DiscussionHidden::class, [Listeners\GiveMoney::class, 'discussionWasHidden'])
+//         ->listen(DiscussionDeleted::class, [Listeners\GiveMoney::class, 'discussionWasDeleted'])
+//         ->listen(Saving::class, [Listeners\GiveMoney::class, 'userWillBeSaved'])
+//         ->listen(Registered::class, [Listeners\GiveMoney::class, 'userWasRegistered']),
+
+//     (new Extend\Routes('api'))
+//         ->get('/money-log', 'money.logs', UserMoneyLogsController::class)
+// ];
+
+// if (class_exists('Flarum\Likes\Event\PostWasLiked')) {
+//     $extend[] =
+//         (new Extend\Event())
+//         ->listen(PostWasLiked::class, [Listeners\GiveMoney::class, 'postWasLiked'])
+//         ->listen(PostWasUnliked::class, [Listeners\GiveMoney::class, 'postWasUnliked']);
+// }
+// return $extend;
